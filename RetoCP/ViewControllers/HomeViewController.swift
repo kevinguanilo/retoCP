8fe4ce9efdd964e74607f2b31b0d060df8e91268
Kevin <kevin160396@gmail.com>
2021-06-09 13:08:39 -0500

    setting general tab bar viewcontroller

---
+++ RetoCP/ViewControllers/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  RetoCP
+//
+//  Created by Kevin Guanilo on 6/9/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
